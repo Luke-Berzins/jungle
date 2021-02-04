044f43f958e187a474ee9069a2f06e6d6a6823f6
luke-berzins <lukeberzins16@gmail.com>
2021-02-04 16:16:05 +0000

    admin dashboard now displays data

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'].to_s, password: ENV['ADMIN_PASSWORD'].to_s
   
   def show
+    @productCount = Product.count
+    @categoryCount = Category.count
   end
   
 end
